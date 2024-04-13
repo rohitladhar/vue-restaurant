a9bd00bca429af4079da305bae370f216e809abc
rohitladhar <rohit.ladhar10@gmail.com>
2024-04-13 11:59:59 +0100

    final deployment

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -32,8 +32,9 @@
             <div>
                 <h2 class="text-2xl font-semibold mb-3">Measures</h2>
                 <ul>
+                    
                     <template v-for="(el,ind) of new Array(20)">
-                        <li v-if="meal[`strMeasure${ind+1}`]">
+                        <li v-if="!!meal[`strMeasure${ind+1}`] && meal[`strMeasure${ind+1}`].trim()">
                             {{ ind+1 }}.{{ meal[`strMeasure${ind+1}`] }}
                         </li>
                     </template>
@@ -61,10 +62,6 @@ import YoutubeButton from '../components/youtubebutton.vue'
 
 const route = useRoute();
 const meal = ref({})
-
-// function stringConvert(event){
-//     return str.replaceAll(",", ", ")
-// }
 
 onMounted(()=>{
     axiosClient.get(`lookup.php?i=${route.params.id}`)
