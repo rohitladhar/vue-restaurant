8c6cd1d9af4594f49e0e2398f6e12e645beeaa32
rohitladhar <rohit.ladhar10@gmail.com>
2024-04-13 12:39:14 +0100

    final deployment

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL,
+    baseURL:'https://www.themealdb.com/api/json/v1/1',
 });
 
 export default axiosClient
